8c90083187502f1a9d5affe9c1f2e4e8e1f72e52
Seth Crist <scrist88@hotmail.com>
2018-02-27 17:43:09 -0500

    Initial Commit

---
+++ Fun Swipt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fun Swipt
+//
+//  Created by Seth Crist on 2/27/18.
+//  Copyright © 2018 Seth Crist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
